e80ca033636a1b8980f706f0c5a3a51b226ecced
Amirxon <shokirovamir2006@gmail.com>
2023-08-04 20:13:01 +0500

    v

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -104,35 +104,42 @@ function Navbar() {
           </div>
         </div>
         <Cart isCartOpened={isCartOpened} setIsCartOpened={setIsCartOpened} />
-        {isMenu && (
-          <div className="nav_mini_menu">
-            <div
-              className="nav_mini_menu_close"
-              onClick={() => {
-                setIsMenu(false);
-              }}
-            >
-              <FontAwesomeIcon icon={faXmark} style={{ color: "#000000" }} />
-            </div>
-            <div className="mini_menu_holder">
-              <div onClick={() => {
-                setIsMenu(false)
-              }} className="mini_menu_link">
-                <NavLink to="/categories/all">categories</NavLink>
-              </div>
-              <div onClick={() => {
-                setIsMenu(false)
-              }} className="mini_menu_link">
-                <NavLink to="/categories/lamps">lamps</NavLink>
-              </div>
-              <div onClick={() => {
-                setIsMenu(false)
-              }} className="mini_menu_link">
-                <NavLink to="/categories/product/19">product page</NavLink>
-              </div>
-            </div>
+
+        <div className={isMenu ? "nav_mini_menu active" : "nav_mini_menu"}>
+          <div
+            className="nav_mini_menu_close"
+            onClick={() => {
+              setIsMenu(false);
+            }}
+          >
+            <FontAwesomeIcon icon={faXmark} style={{ color: "#000000" }} />
           </div>
-        )}
+
+          <div
+            onClick={() => {
+              setIsMenu(false);
+            }}
+            className="mini_menu_link"
+          >
+            <NavLink to="/categories/all">categories</NavLink>
+          </div>
+          <div
+            onClick={() => {
+              setIsMenu(false);
+            }}
+            className="mini_menu_link"
+          >
+            <NavLink to="/categories/lamps">lamps</NavLink>
+          </div>
+          <div
+            onClick={() => {
+              setIsMenu(false);
+            }}
+            className="mini_menu_link"
+          >
+            <NavLink to="/categories/product/19">product page</NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
