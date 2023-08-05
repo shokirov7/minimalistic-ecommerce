242bff4acd6f2f79d6fffa98026de5ff4a697ebb
Amirxon <shokirovamir2006@gmail.com>
2023-08-05 22:07:08 +0500

    gn

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home/Home";
-import Product from "./pages/product/Product";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Loader from "./components/loader/Loader";
 import { Context } from "./context/ThemeContext";
-import All from "./pages/categories/all/All";
-import Furniture from "./pages/categories/furniture/Furniture";
-import Electronics from "./pages/categories/electronics/Electronics";
-import Lamps from "./pages/categories/lamps/Lamps";
-import Kitchen from "./pages/categories/kitchen/Kitchen";
-import Chairs from "./pages/categories/chairs/Chairs";
-import Skin from "./pages/categories/skin/Skin";
+import "./App.css";
+const Home = lazy(() => import("./pages/home/Home"));
+const Product = lazy(() => import("./pages/product/Product"));
+const Navbar = lazy(() => import("./components/navbar/Navbar"));
+const Footer = lazy(() => import("./components/footer/Footer"));
+const Loader = lazy(() => import("./components/loader/Loader"));
+const All = lazy(() => import("./pages/categories/all/All"));
+const Furniture = lazy(() => import("./pages/categories/furniture/Furniture"));
+const Electronics = lazy(() =>
+  import("./pages/categories/electronics/Electronics")
+);
+const Lamps = lazy(() => import("./pages/categories/lamps/Lamps"));
+const Kitchen = lazy(() => import("./pages/categories/kitchen/Kitchen"));
+const Chairs = lazy(() => import("./pages/categories/chairs/Chairs"));
+const Skin = lazy(() => import("./pages/categories/skin/Skin"));
 
 function App() {
   const { pending } = useContext(Context);
@@ -22,18 +24,20 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categories/all" element={<All />} />
-          <Route path="/categories/furnitures" element={<Furniture />} />
-          <Route path="/categories/electronics" element={<Electronics />} />
-          <Route path="/categories/lamps" element={<Lamps />} />
-          <Route path="/categories/kitchen" element={<Kitchen />} />
-          <Route path="/categories/chairs" element={<Chairs />} />
-          <Route path="/categories/skin-care" element={<Skin />} />
-          <Route path="/categories/product/:id" element={<Product />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories/all" element={<All />} />
+            <Route path="/categories/furnitures" element={<Furniture />} />
+            <Route path="/categories/electronics" element={<Electronics />} />
+            <Route path="/categories/lamps" element={<Lamps />} />
+            <Route path="/categories/kitchen" element={<Kitchen />} />
+            <Route path="/categories/chairs" element={<Chairs />} />
+            <Route path="/categories/skin-care" element={<Skin />} />
+            <Route path="/categories/product/:id" element={<Product />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </div>
