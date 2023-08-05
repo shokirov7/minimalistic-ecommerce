0d5d6129c77288fc1bdcdbdf7f2f38d65f8bb507
Amirxon <shokirovamir2006@gmail.com>
2023-08-06 02:49:35 +0500

    g

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -5,7 +5,26 @@ import { Link } from "react-router-dom";
 function Hero() {
   return (
     <div className="hero">
-      <Link to="/categories/furnitures" className="hero_left_block hero_left_block1 hero_block">
+      <Link to="/categories/furnitures" className="hero_block">
+        <h2 className="hero_block_title">Live Comfortably</h2>
+      </Link>
+      <Link to="/categories/skin-care" className="hero_block">
+        <h2 className="hero_block_title">Skincare</h2>
+      </Link>
+      <Link to="/categories/kitchen" className="hero_block">
+        <h2 className="hero_block_title">Kitchen</h2>
+      </Link>
+      <Link to="/categories/electronics" className="hero_block">
+        <h2 className="hero_block_title">Electronics</h2>
+      </Link>
+    </div>
+  );
+}
+
+export default Hero;
+
+{
+  /* <Link to="/categories/furnitures" className="hero_left_block hero_left_block1 hero_block">
         <div className="hero_block_title">Live Comfortably</div>
       </Link>
       <Link to="/categories/skin-care" className="hero_left_block hero_left_block2 hero_block">
@@ -19,9 +38,5 @@ function Hero() {
         <Link to="/categories/electronics" className="hero_right_block hero_right_block2 hero_block">
           <div className="hero_block_title">Electronics</div>
         </Link>
-      </div>
-    </div>
-  );
+      </div> */
 }
-
-export default Hero;
