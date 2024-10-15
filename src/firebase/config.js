886cc31f93fffd64e6e56d6c7dc24932e69a616e
Amirxon <126567111+shokirov7@users.noreply.github.com>
2024-10-15 14:06:50 +0300

    Add files via upload

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,19 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANkgyTSbzxbXsTMwCoxKr4vW1DmhwKCBw",
+  apiKey: "AIzaSyBaz4sXNMXWeS6siQ4jj9E9EM6dRKcShO4",
+
   authDomain: "shop-products-969b2.firebaseapp.com",
+
   projectId: "shop-products-969b2",
+
   storageBucket: "shop-products-969b2.appspot.com",
+
   messagingSenderId: "1066597973683",
-  appId: "1:1066597973683:web:35fa55bfd737e78cd67ac6",
-  measurementId: "G-KLLBL5WVTS",
+
+  appId: "1:1066597973683:web:f52e7b7684449640d67ac6",
+
+  measurementId: "G-BGP1G2FDLZ",
 };
 
 firebase.initializeApp(firebaseConfig);
